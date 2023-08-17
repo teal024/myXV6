deba1c2e59a15c18482d8b6703ff2a21adfa3c07
“teal024” <teal024@foxmail.com>
2023-08-17 23:12:21 +0800

    lab9 done

--- kernel/param.h
+++ kernel/param.h
@@ -15,5 +15,3 @@
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       200000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
-
-
