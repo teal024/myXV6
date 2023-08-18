ebfaa8390845e4931335e195721d203c3f987115
“teal024” <teal024@foxmail.com>
2023-08-18 15:11:26 +0800

    lab1 optional

--- user/find.c
+++ user/find.c
@@ -3,66 +3,85 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-void find(char *path, const char *filename)
-{
-	char buf[512];	  // 保存路径名
-	int fd;			  // 文件描述符
-	struct dirent de; // 目录项
-	struct stat st;	  // 描述文件状态
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
 
-	if ((fd = open(path, 0)) < 0)
-	{
-		fprintf(2, "ls: cannot open %s\n", path);
+int match(char *re, char *text) {
+	if(re[0] == '^')
+		return matchhere(re+1, text);
+	do{  // must look at empty string
+		if(matchhere(re, text))
+		return 1;
+	}while(*text++ != '\0');
+	return 0;
+}
+
+// lab1 Optinal
+int matchhere(char *re, char *text) {
+	if(re[0] == '\0')
+		return 1;
+	if(re[1] == '*')
+		return matchstar(re[0], re+2, text);
+	if(re[0] == '$' && re[1] == '\0')
+		return *text == '\0';
+	if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+		return matchhere(re+1, text+1);
+	return 0;
+}
+
+int matchstar(int c, char *re, char *text)
+{
+	do{
+		if(matchhere(re, text))
+			return 1;
+	}while(*text!='\0' && (*text++==c || c=='.'));
+	return 0;
+}
+
+void find(char *path, char *target) {
+	char buf[512], *p;
+	int fd;
+	struct dirent de;
+	struct stat st;
+
+	if((fd = open(path, 0)) < 0){
+		fprintf(2, "find: cannot open %s\n", path);
 		return;
 	}
 
-	if (fstat(fd, &st) < 0)
-	{ // 是否成功获取fd的状态, fstat根据文件描述符来获取状态，stat根据路径来获取状态
-		fprintf(2, "ls: cannot stat %s\n", path);
+	if(fstat(fd, &st) < 0){
+		fprintf(2, "find: cannot stat %s\n", path);
 		close(fd);
 		return;
 	}
 
-	switch (st.type)
-	{
-	// find 不考虑find一个文件，只考虑find一个目录
+	switch(st.type){
 	case T_FILE:
-		//   printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+		if(match(target, path)) {
+			printf("%s\n", path);
+		}
 		break;
-
 	case T_DIR:
-		if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
-		{
+		// printf("%s\n", path);
+		if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
 			printf("find: path too long\n");
 			break;
 		}
-		// 如果缓冲区大小能够容纳
-		strcpy(buf, path);			 // 先把path存存放到buf中
-		char *p = buf + strlen(buf); // p从buf后面开始
-		*p++ = '/';					 // p后面一位直接用'/'来代表，p指针指向下一位
-		while (read(fd, &de, sizeof(de)) == sizeof(de))
-		{ // 每次读取一个目录项
-			// 从文件描述符 fd 所指向的文件中读取数据，并将读取的数据保存在 de 结构体中
-			// 与sizeof(de)比较是为了比较是否相等，如果不相等就不继续
-			if (de.inum == 0) // 检查目录项是否有效
+		strcpy(buf, path);
+		p = buf+strlen(buf);
+		*p++ = '/';
+		while(read(fd, &de, sizeof(de)) == sizeof(de)){
+			if(de.inum == 0)
 				continue;
-			memmove(p, de.name, DIRSIZ); // 将目录项 de 中的目录/文件名(de.name)复制到缓冲区 p 中
-			p[DIRSIZ] = 0;				 // 添加终止符
-			if (stat(buf, &st) < 0)
-			{ // 获取buf状态是否失败，一般就在打印buf有关信息前用下即可
+			memmove(p, de.name, DIRSIZ);
+			p[DIRSIZ] = 0;
+			if(stat(buf, &st) < 0){
 				printf("find: cannot stat %s\n", buf);
 				continue;
 			}
-			if (st.type == T_DIR)
-			{
-				if (strcmp(p, ".") == 0 || strcmp(p, "..") == 0)
-					continue; // 避免在"."以及".."中发生递归
-				find(buf, filename);
-			}
-			else if (st.type == T_FILE)
-			{
-				if (strcmp(filename, p) == 0)
-					printf("%s\n", buf);    // 这里打印的是buf，也即完整的路径
+	//   printf("%s\n", buf);
+			if(strcmp(buf+strlen(buf)-2, "/.") != 0 && strcmp(buf+strlen(buf)-3, "/..") != 0) {
+				find(buf, target);
 			}
 		}
 		break;
@@ -72,10 +91,12 @@ void find(char *path, const char *filename)
 
 int main(int argc, char *argv[])
 {
-	if(argc != 3){
-    printf("argument number error\n");
-    exit(1);
-  }
-  find(argv[1], argv[2]);
-  exit(0);
+	if(argc < 3){
+		exit(0);
+	}
+	char target[512];
+	target[0] = '/';
+	strcpy(target+1, argv[2]);
+	find(argv[1], target);
+	exit(0);
 }
